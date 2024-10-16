5504f4cf4ebbf41a5064fb6259502a7765a9b864
Tumi Wang <lianyuguhun@gmail.com>
2024-10-16 20:06:33 +0800

    修改测试程序test14_1

--- source/test14_1.cpp
+++ source/test14_1.cpp
@@ -1,20 +1,6 @@
 #include <cstdint>
 #include <iostream>
 
-template<typename T>
-void PrintMemory(T& v)
-{
-    const auto size = sizeof(v);
-    // std::cout << typeid(T).name() << " of the Value(" << v;
-    // std::cout << ") use memory:";
-    std::cout << std::hex;
-    const char* p =(const char*)(&v);
-    for(int i = 0; i < size; ++i) {
-        std::cout << (unsigned char)(*(p + i));
-    }
-    std::cout << std::dec;
-}
-
 int main()
 {
     std::cout << "size of int16_t: " << sizeof(std::int16_t) << std::endl;
@@ -25,5 +11,4 @@ int main()
     std::int_fast16_t a2 = 33000;
     std::int_least16_t a3 = 33000;
     std::cout << "a1=" << a1 << " a2=" << a2 << " a3=" << a3 << std::endl;
-    PrintMemory(a2);
 }
