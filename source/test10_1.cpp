b1a264e39f58f6953862db4fd0044cd681da936b
Tumi Wang <lianyuguhun@gmail.com>
2024-10-18 15:27:48 +0800

    修改测试test10_1

--- source/test10_1.cpp
+++ source/test10_1.cpp
@@ -3,24 +3,24 @@
 class A
 {
 public:
-    void test() const {
-        std::cout << "Call test() with const" << std::endl;
+    void test(const int lineno) const {
+        std::cout << "line:" << lineno << " Call test() with const" << std::endl;
     }
-    void test() {
-        std::cout << "Call test() without const" << std::endl;
+    void test(const int lineno) {
+        std::cout  << "line:" << lineno << " Call test() without const" << std::endl;
     }
 };
 
 int main()
 {
     A a0;
-    a0.test();
+    a0.test(__LINE__);
     const A a1;
-    a1.test();
+    a1.test(__LINE__);
     auto a2 = a1;
-    a2.test();
+    a2.test(__LINE__);
     decltype(a1) a3 = a1;
-    a3.test();
+    a3.test(__LINE__);
     decltype(a0) a4 = a0;
-    a4.test();
+    a4.test(__LINE__);
 }
